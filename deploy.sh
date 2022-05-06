5ddf811877ef41c997750d14c6b529d021203f79
Jose Neto <joca318@gmail.com>
2022-05-06 08:15:13 -0300

    'first commit - main'

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#1
+gcloud deploy delivery-pipelines delete boutique-demo-app01 --region us-central1 --force
+
+#2
+gcloud deploy apply --file=clouddeploy.yaml --region=us-central1
+
+#3
+gcloud deploy releases create boutique-enabling-tracing-v01 --source="." --skaffold-file=skaffold-deploy.yaml --skaffold-version=1.35 --delivery-pipeline=boutique-demo-app01 --region us-central1
